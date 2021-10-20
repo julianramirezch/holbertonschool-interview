44a5cdadb8518d0f42d5b6e8cd191ed14b6296ad
julianramirezch <julianramirezch1@gmail.com>
2021-10-20 08:32:29 -0500

    [add]: n queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,79 +1,73 @@
 #!/usr/bin/python3
-"""
-N queens puzzle is the challenge of placing N non-attacking queens
-"""
+""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
 import sys
 
 
-def printB(brd):
-    """
-    print board
-    """
-    r = []
-    for x in brd:
-        for c in x:
-            if c == 1:
-                r.append([brd.index(x), x.index(c)])
-    print(r)
+class NQueen:
+    """ Class for solving N Queen Problem """
+
+    def __init__(self, n):
+        """ Global Variables """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ Checks if k Queen can be placed in i column (True)
+        or if the are attacking queens in row or diagonal (False)
+        """
+
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Tries to place every queen in the board
+        Args:
+        k: starting queen from which to evaluate (should be 1)
+        """
+        # i goes from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    # Need to place more Queens
+                    self.nQueen(k + 1)
+        return self.res
 
 
-def is_safe(brd, row, col, n):
-    """
-    is_safe
-    """
-    for i in range(col):
+# Main
 
-        if brd[row][i] + brd[row][i + 1] != 0:
-            return False
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
+N = sys.argv[1]
 
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    return True
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
+queen = NQueen(N)
+res = queen.nQueen(1)
 
-def nqueen(brd, col, n):
-    """
-    nqueen
-    """
-
-    if (col >= n):
-        printB(brd)
-
-    for x in range(n):
-        if is_safe(brd, x, col, n):
-            brd[x][col] = 1
-            if nqueen(brd, col+1, n):
-                return True
-            brd[x][col] = 0
-
-    return False
-
-
-# initial run starting from the col 0!
-def main():
-    """
-    Main
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if sys.argv[1].isnumeric():
-        n = int(sys.argv[1])
-    else:
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-    brd = [[0 for x in range(n)] for y in range(n)]
-    nqueen(brd, 0, n)
-
-
-if __name__ == '__main__':
-    main()
+for i in res:
+    print(i)
